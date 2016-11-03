312f0b4881a76d4c6c0366af9be308975ccba77c
Squeaky <squeaky_pl@gmx.com>
2016-11-03 00:08:13 -0200

    add render method to C response

--- responses/cresponse.c
+++ responses/cresponse.c
@@ -8,6 +8,8 @@ typedef struct {
   PyObject* mime_type;
   PyObject* text;
   PyObject* encoding;
+
+  char buffer[1024];
 } Response;
 
 static PyObject *
@@ -40,13 +42,145 @@ Response_dealloc(Response* self)
   Py_TYPE(self)->tp_free((PyObject*)self);
 }
 
+static const char header[] = "HTTP/1.1 200 OK\r\n"
+  "Connection: keep-alive\r\n"
+  "Content-Length: ";
+static const size_t code_offset = 9;
 
 static int
-Response_init(Response* self, PyObject *args, PyObject *kwds)
+Response_init(Response* self, PyObject *args, PyObject *kw)
 {
+  static char *kwlist[] = {"status_code", "text", "mime_type", "encoding", NULL};
+
   // FIXME: check argument types
+  if (!PyArg_ParseTupleAndKeywords(
+      args, kw, "|OOOO", kwlist,
+      &self->status_code, &self->text, &self->mime_type, &self->encoding))
+      goto error;
+
+  if(self->status_code) {
+    Py_INCREF(self->status_code);
+  }
+  if(self->text) {
+    Py_INCREF(self->text);
+  }
+  if(self->mime_type) {
+    Py_INCREF(self->mime_type);
+  }
+  if(self->encoding) {
+    Py_INCREF(self->encoding);
+  }
+
+  memcpy(self->buffer, header, strlen(header));
+
+  goto finally;
+
+  error:
+  return -1;
+  finally:
   return 0;
 }
+
+
+static const char Content_Type[] = "Content-Type: ";
+static const char encoding_c[] = "; encoding=";
+static const char utf8[] = "utf-8";
+static const char text_plain[] = "text/plain";
+
+static PyObject*
+Response_render(Response* self)
+{
+  if(self->status_code) {
+    unsigned long status_code = PyLong_AsUnsignedLong(self->status_code);
+    /* FIXME: overflow, check range 100 - 599 */
+    /* TODO these are always 3 digit, maybe modulus would be faster */
+    snprintf(self->buffer + code_offset, 4, "%ld", status_code);
+    *(self->buffer + code_offset + 3) = ' ';
+  }
+
+  size_t buffer_offset = strlen(header);
+
+  Py_ssize_t body_len = 0;
+  const char* body = NULL;
+  if(self->text) {
+    if(!self->encoding) {
+      body = PyUnicode_AsUTF8AndSize(self->text, &body_len);
+      if(!body)
+        goto error;
+    } else {
+      /* TODO handle other encodings */
+    }
+
+    int result = sprintf(
+      self->buffer + buffer_offset, "%ld", (unsigned long)body_len);
+    buffer_offset += result;
+  } else {
+    *(self->buffer + buffer_offset) = '0';
+    buffer_offset++;
+  }
+
+# define CRLF \
+  *(self->buffer + buffer_offset) = '\r'; \
+  buffer_offset++; \
+  *(self->buffer + buffer_offset) = '\n'; \
+  buffer_offset++;
+
+  CRLF
+
+  memcpy(self->buffer + buffer_offset, Content_Type, strlen(Content_Type));
+  buffer_offset += strlen(Content_Type);
+
+  Py_ssize_t mime_type_len = strlen(text_plain);
+  const char* mime_type = text_plain;
+  if(self->mime_type) {
+    mime_type = PyUnicode_AsUTF8AndSize(self->mime_type, &mime_type_len);
+    if(!mime_type)
+      goto error;
+
+  }
+  memcpy(self->buffer + buffer_offset, mime_type, (size_t)mime_type_len);
+  buffer_offset += mime_type_len;
+  memcpy(self->buffer + buffer_offset, encoding_c, strlen(encoding_c));
+  buffer_offset += strlen(encoding_c);
+
+  Py_ssize_t encoding_len = strlen(utf8);
+  const char* encoding = utf8;
+  if(self->encoding) {
+    encoding = PyUnicode_AsUTF8AndSize(self->encoding, &encoding_len);
+    if(!encoding)
+      goto error;
+  }
+  memcpy(self->buffer + buffer_offset, encoding, (size_t)encoding_len);
+  buffer_offset += (size_t)encoding_len;
+
+  CRLF
+  CRLF
+
+  if(body) {
+    memcpy(self->buffer + buffer_offset, body, (size_t)body_len);
+    buffer_offset += (size_t)body_len;
+  }
+
+#undef CRLF
+
+  /* FIXME we should implement buffer protocol instead */
+  PyObject* view = PyMemoryView_FromMemory(
+    self->buffer, buffer_offset, PyBUF_READ);
+  if(!view)
+    goto error;
+
+  return view;
+
+  error:
+    return NULL;
+}
+
+
+static PyMethodDef Response_methods[] = {
+  {"render", (PyCFunction)Response_render, METH_NOARGS, "render"},
+  {NULL}
+};
+
 
 static PyTypeObject ResponseType = {
   PyVarObject_HEAD_INIT(NULL, 0)
@@ -76,7 +210,7 @@ static PyTypeObject ResponseType = {
   0,                         /* tp_weaklistoffset */
   0,                         /* tp_iter */
   0,                         /* tp_iternext */
-  0,                         /* tp_methods */
+  Response_methods,          /* tp_methods */
   0,                         /* tp_members */
   0,                         /* tp_getset */
   0,                         /* tp_base */
