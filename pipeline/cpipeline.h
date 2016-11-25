399fbd78c6b8e15afee8e39b812c25d188116d60
Squeaky <squeaky_pl@gmx.com>
2016-11-25 05:26:29 -0200

    Forgotten header file

---
+++ pipeline/cpipeline.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <Python.h>
+
+
+typedef struct {
+  PyObject_HEAD
+#ifdef PIPELINE_OPAQUE
+  PyObject* ready;
+#else
+  void* (*ready)(PyObject*, void*);
+  void* ready_closure;
+#endif
+  PyObject* task_done;
+  PyObject* queue[10];
+  size_t queue_start;
+  size_t queue_end;
+} Pipeline;
+
+
+#define PIPELINE_EMPTY(p) (p)->queue_start == (p)->queue_end
+
+#ifndef PIPELINE_OPAQUE
+PyObject*
+Pipeline_new(Pipeline* self);
+
+void
+Pipeline_dealloc(Pipeline* self);
+
+int
+Pipeline_init(Pipeline* self, void* (*ready)(PyObject*, void*), void* closure);
+
+PyObject*
+Pipeline_queue(Pipeline* self, PyObject* task);
+
+void*
+cpipeline_init(void);
+#endif
