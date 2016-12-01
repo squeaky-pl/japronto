e0b17f715810411b63d9f7b97b5628ae4bbe4dbf
Squeaky <squeaky_pl@gmx.com>
2016-12-01 13:29:48 -0200

    shut up more buggers

--- buggers.py
+++ buggers.py
@@ -1,9 +1,10 @@
 import atexit
 import subprocess
 
+noisy = ['atom', 'chrome', 'firefox', 'dropbox']
 
 def silence():
-    subprocess.call(['pkill', '--signal', 'STOP', 'firefox'])
+    subprocess.call(['pkill', '--signal', 'STOP', '|'.join(noisy)])
     def noise():
-        subprocess.call(['pkill', '--signal', 'CONT', 'firefox'])
+        subprocess.call(['pkill', '--signal', 'CONT', '|'.join(noisy)])
     atexit.register(noise)
