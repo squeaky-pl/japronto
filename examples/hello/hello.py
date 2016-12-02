83a02389ea54ef222856c2d33d0a434538b7dc0a
Squeaky <squeaky_pl@gmx.com>
2016-12-02 17:05:52 -0200

    simplit out the hello example for testing

---
+++ examples/hello/hello.py
@@ -0,0 +1,18 @@
+import os.path
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + '/../..'))
+
+from app import Application
+
+
+def hello(request):
+    return request.Response(text='Hello world!')
+
+
+app = Application()
+
+r = app.get_router()
+r.add_route('/', hello)
+
+app.serve()
