2dde26475322bcd655d8f37f11cd249338380d10
Squeaky <squeaky_pl@gmx.com>
2016-12-18 01:54:18 -0200

    add hypothesis integration test for method and match_dict

--- integration_tests/test_integr.py
+++ integration_tests/test_integr.py
@@ -6,6 +6,9 @@ import socket
 import psutil
 import time
 import json
+import urllib.parse
+import string
+from hypothesis import given, strategies as st, settings, Verbosity
 
 
 @pytest.fixture(autouse=True)
@@ -21,20 +24,42 @@ def server():
 
     yield server
     server.terminate()
+    server.wait()
+
+    assert server.returncode == 0
 
 
-@pytest.fixture
-def connection():
+def connect():
     return urllib3.connection.HTTPConnection('localhost:8080')
 
 
-def test(connection):
-    connection.request('GET', '/dump/1/2')
+method_alphabet = string.digits + string.ascii_letters + string.punctuation
+@given(method=st.text(method_alphabet, min_size=1))
+@settings(verbosity=Verbosity.verbose)
+def test_method(method):
+    connection = connect()
+    connection.request(method, '/dump/1/2')
     response = connection.getresponse()
     json_body = json.loads(response.read().decode('utf-8'))
-    print(json_body)
 
-    connection.request('GET', '/dump/3/4')
+    assert response.status == 200
+    assert json_body['method'] == method
+
+    connection.close()
+
+
+param_alphabet = st.characters(blacklist_characters='/?') \
+    .filter(lambda x: not any(0xD800 <= ord(c) <= 0xDFFF for c in x))
+param = st.text(param_alphabet, min_size=1)
+@given(param1=param, param2=param)
+@settings(verbosity=Verbosity.verbose)
+def test_match_dict(param1, param2):
+    connection = connect()
+    connection.request('GET', urllib.parse.quote('/dump/{}/{}'.format(param1, param2)))
     response = connection.getresponse()
     json_body = json.loads(response.read().decode('utf-8'))
-    print(json_body)
+
+    assert response.status == 200
+    assert json_body['match_dict'] == {'p1': param1, 'p2': param2}
+
+    connection.close()
