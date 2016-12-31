4a6bf6cbcd0050c65791bd3618685b4db39e0725
Squeaky <squeaky_pl@gmx.com>
2016-12-31 05:21:12 -0200

    add support for tests with PYTHONPATH

--- conftest.py
+++ conftest.py
@@ -5,6 +5,7 @@ import shutil
 
 
 def pytest_configure(config):
+    os.putenv('PYTHONPATH', 'src')
     subprocess.check_call([sys.executable, 'build.py', '--coverage'])
 
     subprocess.check_call([
@@ -23,7 +24,7 @@ def pytest_unconfigure(config):
         'build/temp.linux-x86_64-3.5', '-c', '-o', 'coverage.info', '-q'])
     subprocess.check_call([
         'lcov', '--remove', 'coverage.info',
-        '/usr*', '-o', 'coverage.info', '-q']) 
+        '/usr*', '-o', 'coverage.info', '-q'])
 
     shutil.rmtree('test_coverage')
 
