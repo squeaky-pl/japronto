9585b706c5483b34be64c9a8159e920c6f8e48c1
Squeaky <squeaky_pl@gmx.com>
2017-01-01 10:17:18 -0200

    adjust lcov paths

--- conftest.py
+++ conftest.py
@@ -14,7 +14,7 @@ def global_fixture():
 
     subprocess.check_call([
         'lcov', '--base-directory', '.', '--directory',
-        'build/temp.linux-x86_64-3.5', '--zerocounters', '-q'])
+        '.test/.build/temp', '--zerocounters', '-q'])
 
     yield
 
@@ -25,7 +25,7 @@ def global_fixture():
 
     subprocess.check_call([
         'lcov', '--base-directory', '.', '--directory',
-        'build/temp.linux-x86_64-3.5', '-c', '-o', 'coverage.info', '-q'])
+        '.test/.build/temp', '-c', '-o', 'coverage.info', '-q'])
     subprocess.check_call([
         'lcov', '--remove', 'coverage.info',
         '/usr*', '-o', 'coverage.info', '-q'])
