9703591c14da67edd5116443118d802859ee7905
Squeaky <squeaky_pl@gmx.com>
2017-01-04 06:59:06 -0200

    add reaper tests

---
+++ integration_tests/reaper.py
@@ -0,0 +1,13 @@
+import sys
+
+from app import Application
+
+reaper_settings = {
+    'check_interval': int(sys.argv[1]),
+    'idle_timeout': int(sys.argv[2])
+}
+
+app = Application(reaper_settings=reaper_settings)
+
+if __name__ == '__main__':
+    app.serve()
---
+++ integration_tests/test_reaper.py
@@ -0,0 +1,66 @@
+from functools import partial
+import subprocess
+import sys
+import time
+
+import psutil
+import pytest
+
+import client
+
+
+@pytest.fixture(scope='function', params=[2, 3, 4])
+def get_connections_and_wait(request):
+    server = subprocess.Popen([
+        sys.executable, 'integration_tests/reaper.py', '1', str(request.param)])
+    proc = psutil.Process(server.pid)
+
+    assert server.poll() is None
+
+    # wait until the server socket is open
+    try:
+        while 1:
+            if proc.connections():
+                break
+        time.sleep(.001)
+    except psutil.AccessDenied:
+        time.sleep(.2)
+
+    yield lambda: len(proc.connections()), partial(time.sleep, request.param)
+
+    server.terminate()
+    server.wait()
+    assert server.returncode == 0
+
+
+def test_empty(get_connections_and_wait):
+    get_connections, wait = get_connections_and_wait
+    conn = client.Connection('localhost:8080')
+
+    assert get_connections() == 1
+
+    conn.maybe_connect()
+    time.sleep(.1)
+
+    assert get_connections() == 2
+
+    wait()
+
+    assert get_connections() == 1
+
+
+def test_request(get_connections_and_wait):
+    get_connections, wait = get_connections_and_wait
+    conn = client.Connection('localhost:8080')
+
+    assert get_connections() == 1
+
+    conn.putrequest('GET', '/')
+    conn.endheaders()
+
+    assert get_connections() == 2
+
+    wait()
+    time.sleep(1)
+    
+    assert get_connections() == 1
