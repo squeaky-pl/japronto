f9c0370b1577fd34a1d4d794da5d61ac1897560b
Squeaky <squeaky_pl@gmx.com>
2017-01-09 14:08:35 -0200

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt-get install python3 git libbz2-dev libz-dev libsqlite3-dev libssl-dev gcc make libffi-dev lcov
+sudo apt-get update
+sudo apt-get install -y python3 git libbz2-dev libz-dev libsqlite3-dev libssl-dev gcc make libffi-dev lcov
 git clone https://github.com/yyuu/pyenv .pyenv
 .pyenv/bin/pyenv install -v 3.6.0
 wget https://pypi.python.org/packages/d4/0c/9840c08189e030873387a73b90ada981885010dd9aea134d6de30cd24cb8/virtualenv-15.1.0.tar.gz
