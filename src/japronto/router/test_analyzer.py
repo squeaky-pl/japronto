6a8d3e0485e5b3fa2043974bc6261a9838990812
Squeaky <squeaky_pl@gmx.com>
2017-01-20 18:59:16 -0200

    add a test for is_pointless_coroutine

--- src/japronto/router/test_analyzer.py
+++ src/japronto/router/test_analyzer.py
@@ -5,7 +5,7 @@ import pytest
 from . import analyzer
 
 
-fixtures = OrderedDict([
+simple_fixtures = OrderedDict([
     ('empty', ('def a(): pass', False)),
     ('arg', ('def a(b): return b', False)),
     ('simple', ('def a(c): return c.Response()', True)),
@@ -28,13 +28,28 @@ def a(b):
     return b.Response(json={c: d})
         ''', True))
 ])
-params = fixtures.values()
-ids = list(fixtures.keys())
 
 
-@pytest.mark.parametrize('code,simple', params, ids=ids)
-def test_analyzer(code, simple):
+@pytest.mark.parametrize(
+    'code,simple', simple_fixtures.values(), ids=list(simple_fixtures.keys()))
+def test_is_simple(code, simple):
     module = compile(code, '?', 'exec')
     fun_code = module.co_consts[0]
 
     assert analyzer.is_simple(fun_code) == simple
+
+
+pointless_fixtures = OrderedDict([
+    ('empty', ('async def a(): pass', True)),
+    ('simple', ('async def a(): return 1', True)),
+    ('yieldfrom', ('def a(b): yield from b', False)),
+    ('await', ('async def a(b): await b', False))
+])
+@pytest.mark.parametrize(
+    'code,pointless', pointless_fixtures.values(),
+    ids=list(pointless_fixtures.keys()))
+def test_is_pointless(code, pointless):
+    module = compile(code, '?', 'exec')
+    fun_code = module.co_consts[0]
+
+    assert analyzer.is_pointless_coroutine(fun_code) == pointless
