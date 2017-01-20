4ebaa8bd1a2fdc46d6ca4c5bbd6d2d05530b8a39
Squeaky <squeaky_pl@gmx.com>
2017-01-20 18:51:15 -0200

    allow needed logic to detect if something is a pointless coroutine

--- src/japronto/router/analyzer.py
+++ src/japronto/router/analyzer.py
@@ -1,4 +1,8 @@
 import dis
+import functools
+
+
+FLAG_COROUTINE = 128
 
 
 def is_simple(fun):
@@ -24,3 +28,29 @@ def is_simple(fun):
             continue
 
     return seen_call_fun and seen_load_fast_0 and seen_load_response
+
+
+def is_pointless_coroutine(fun):
+    for instruction in dis.get_instructions(fun):
+        if instruction.opname in ('GET_AWAITABLE', 'YIELD_FROM'):
+            return False
+
+    return True
+
+
+def coroutine_to_func(f):
+    """Based on http://stackoverflow.com/questions/13503079/how-to-create-a-copy-of-a-python-function"""
+    oc = f.__code__
+    code = types.CodeType(
+        oc.co_argcount, oc.co_kwonlyargcount, oc.co_nlocals, oc.co_stacksize,
+        oc.co_flags & ~FLAG_COROUTINE,
+        oc.co_code, oc.co_consts, oc.co_names, oc.co_varnames, oc.co_filename,
+        oc.co_name, oc.co_firstlineno, oc.co_lnotab, oc.co_freevars,
+        oc.co_cellvars)
+    g = types.FunctionType(
+        code, f.__globals__, name=f.__name__, argdefs=f.__defaults__,
+        closure=f.__closure__)
+    g = functools.update_wrapper(g, f)
+    g.__kwdefaults__ = f.__kwdefaults__
+
+    return g
