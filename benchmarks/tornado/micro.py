a6f8e25d1853df42c6c7a2b898b72e42b008e511
Squeaky <squeaky_pl@gmx.com>
2017-01-22 06:34:14 -0200

    make tornado benchmark fairer

--- benchmarks/tornado/micro.py
+++ benchmarks/tornado/micro.py
@@ -1,21 +1,27 @@
 from tornado import ioloop, web
+from tornado.httputil import HTTPHeaders, responses
 
 
 class MainHandler(web.RequestHandler):
     def get(self):
         self.write('Hello world!')
 
-    # skip calculating ETag, makes tornado ~8% faster
+    # skip calculating ETag, ~8% faster
     def set_etag_header(self):
          pass
 
     def check_etag_header(self):
          return False
 
+    # torando sends Server and Date headers by default, ~4% faster
+    def clear(self):
+        self._headers = HTTPHeaders(
+            {'Content-Type': 'text/plain; charset=utf-8'})
+        self._write_buffer = []
+        self._status_code = 200
+        self._reason = responses[200]
 
-app = web.Application([('/', MainHandler)], debug=False,
-    compress_response=False,
-    static_hash_cache=False)
+app = web.Application([('/', MainHandler)])
 
 app.listen(8080)
 ioloop.IOLoop.current().start()
