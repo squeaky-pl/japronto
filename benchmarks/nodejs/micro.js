7fdb8218bf9486da8490c02c0171cfe98fea5c44
Squeaky <squeaky_pl@gmx.com>
2017-01-26 02:35:29 -0200

    make nodejs equal

--- benchmarks/nodejs/micro.js
+++ benchmarks/nodejs/micro.js
@@ -3,8 +3,14 @@ const http = require('http');
 
 var srv = http.createServer( (req, res) => {
   res.sendDate = false;
-  data = 'Hello world!'
-  res.writeHead(200, {
+  if(req.url == '/') {
+    data = 'Hello world!'
+    status = 200
+  } else {
+    data = 'Not Found'
+    status = 404
+  }
+  res.writeHead(status, {
     'Content-Type': 'text/plain; encoding=utf-8',
     'Content-Length': data.length});
   res.end(data);
