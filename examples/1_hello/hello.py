15b848b0c694b2a692d3e5ba789389cb05f478c6
Squeaky <squeaky_pl@gmx.com>
2017-01-29 20:12:52 -0200

    add comments to hello example

--- examples/1_hello/hello.py
+++ examples/1_hello/hello.py
@@ -1,13 +1,21 @@
 from japronto import Application
 
 
+# Views handle logic, take request as a parameter and
+# returns Response object back to the client
 def hello(request):
     return request.Response(text='Hello world!')
 
 
+# The Application instance is a fundamental concept.
+# It is a parent to all the resources and all the settings
+# can be tweaked here.
 app = Application()
 
-r = app.router
+# The Router instance lets you register your handlers and execute
+# them depending on the url path and methods
 r.add_route('/', hello)
 
-app.run()
+# Finally start our server and handle requests until termination is
+# requested. Enabling debug lets you see request logs and stack traces.
+app.run(debug=True)
