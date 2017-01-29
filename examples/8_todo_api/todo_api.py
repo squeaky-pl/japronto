cf26e49d27d539b771cec5da84dfae060ce4fbc9
Squeaky <squeaky_pl@gmx.com>
2017-01-29 00:26:52 -0200

    move extend and todo api examples

--- examples/8_todo_api/todo_api.py
+++ examples/8_todo_api/todo_api.py
@@ -6,30 +6,6 @@ import json
 
 from japronto import Application
 
-DB_FILE = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), 'todo.sqlite'))
-db_connect = partial(sqlite3.connect, DB_FILE)
-
-
-def maybe_create_schema():
-    db = db_connect()
-    db.execute("""
-        CREATE TABLE IF NOT EXISTS todos
-        (id INTEGER PRIMARY KEY, todo TEXT)""")
-    db.close()
-
-
-def cursor(request):
-    def done_cb(request):
-        request.extra['conn'].close()
-
-    if not 'conn' in request.extra:
-        request.extra['conn'] = db_connect()
-        request.add_done_callback(done_cb)
-
-    return request.extra['conn'].cursor()
-
-
 
 def add_todo(request):
     cur = request.cursor
@@ -55,7 +31,7 @@ def show_todo(request):
     cur.execute("""SELECT id, todo FROM todos WHERE id = ?""", (id,))
     todo = cur.fetchone()
     if not todo:
-        return request.Response(code=404, json={})
+        return request.Response(code=404, json={"error": "not found"})
     todo = {"id": todo[0], "todo": todo[1]}
 
     return request.Response(json=todo)
@@ -66,14 +42,40 @@ def delete_todo(request):
     id = int(request.match_dict['id'])
     cur.execute("""DELETE FROM todos WHERE id = ?""", (id,))
     if not cur.rowcount:
-        return request.Response(code=404, json={})
+        return request.Response(code=404, json={"error": "not found"})
     cur.connection.commit()
 
     return request.Response(json={})
 
 
+DB_FILE = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), 'todo.sqlite'))
+db_connect = partial(sqlite3.connect, DB_FILE)
+
+
+def maybe_create_schema():
+    db = db_connect()
+    db.execute("""
+        CREATE TABLE IF NOT EXISTS todos
+        (id INTEGER PRIMARY KEY, todo TEXT)""")
+    db.close()
+
+
 maybe_create_schema()
 app = Application()
+
+
+def cursor(request):
+    def done_cb(request):
+        request.extra['conn'].close()
+
+    if not 'conn' in request.extra:
+        request.extra['conn'] = db_connect()
+        request.add_done_callback(done_cb)
+
+    return request.extra['conn'].cursor()
+
+
 app.extend_request(cursor, property=True)
 router = app.router
 router.add_route('/todos', list_todos, method='GET')
