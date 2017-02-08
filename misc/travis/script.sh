8ffe09500ecf567e1701a381472d99f89f29451e
Squeaky <squeaky_pl@gmx.com>
2017-02-08 12:51:30 -0200

    [travis-wheel] shell for syntax is too hard for me

--- misc/travis/script.sh
+++ misc/travis/script.sh
@@ -4,7 +4,7 @@ set -ex
 
 if [[ $JAPR_WHEEL == "1" ]]; then
   if [[ $JAPR_OS == "Linux" ]]; then
-    for PYTHON_TAG in "cp35-cp35m cp36-cp36m"; do
+    for PYTHON_TAG in cp35-cp35m cp36-cp36m; do
       docker run --rm -u `id -u` -w /io -v `pwd`:/io quay.io/pypa/manylinux1_x86_64 /opt/python/$PYTHON_TAG/bin/python setup.py bdist_wheel
       docker run --rm -u `id -u` -w /io -v `pwd`:/io quay.io/pypa/manylinux1_x86_64 auditwheel repair dist/*-$PYTHON_TAG-linux_x86_64.whl
       unzip -l wheelhouse/*-$PYTHON_TAG-manylinux1_x86_64.whl
