32f160fbaedafc5eb599bff98a90058731ce4624
Squeaky <squeaky_pl@gmx.com>
2017-02-08 13:11:57 -0200

    [travis-wheel] simplify

--- misc/travis/before_install.sh
+++ misc/travis/before_install.sh
@@ -3,4 +3,11 @@
 export JAPR_MSG=`git show -s --format=%B | xargs`
 export JAPR_WHEEL=`[[ $JAPR_MSG == *"[travis-wheel]"* ]] && echo 1 || echo 0`
 export JAPR_OS=`uname`
+
+if [[ $VERSION == "3.5."* ]]; then
+  export PYTHON_TAG=cp35-cp35m
+elif [[ $VERSION == "3.6."* ]]; then
+  export PYTHON_TAG=cp36-cp36m
+fi
+
 env | grep "^JAPR_"
--- misc/travis/script.sh
+++ misc/travis/script.sh
@@ -4,20 +4,16 @@ set -ex
 
 if [[ $JAPR_WHEEL == "1" ]]; then
   if [[ $JAPR_OS == "Linux" ]]; then
-    for PYTHON_TAG in cp35-cp35m cp36-cp36m; do
-      docker run --rm -u `id -u` -w /io -v `pwd`:/io quay.io/pypa/manylinux1_x86_64 /opt/python/$PYTHON_TAG/bin/python setup.py bdist_wheel
-      docker run --rm -u `id -u` -w /io -v `pwd`:/io quay.io/pypa/manylinux1_x86_64 auditwheel repair dist/*-$PYTHON_TAG-linux_x86_64.whl
-      unzip -l wheelhouse/*-$PYTHON_TAG-manylinux1_x86_64.whl
-    done
-    rm -f dist/*
+    docker run --rm -u `id -u` -w /io -v `pwd`:/io quay.io/pypa/manylinux1_x86_64 /opt/python/$PYTHON_TAG/bin/python setup.py bdist_wheel
+    docker run --rm -u `id -u` -w /io -v `pwd`:/io quay.io/pypa/manylinux1_x86_64 auditwheel repair dist/*-$PYTHON_TAG-linux_x86_64.whl
     cp wheelhouse/*.whl dist
   fi
 
   if [[ $JAPR_OS == "Darwin" ]]; then
     python setup.py bdist_wheel
-    ls -lha dist
-    unzip -l dist/*.whl
   fi
 
+  ls -lha dist
+  unzip -l dist/*.whl
   twine upload -u squeaky dist/*.whl
 fi
