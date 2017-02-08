3d4c5b67a65cdf0b64d9454746ce305179bf09a3
Squeaky <squeaky_pl@gmx.com>
2017-02-08 12:22:31 -0200

    [travis-wheel] pass wheels separately to auditwheel

--- misc/travis/script.sh
+++ misc/travis/script.sh
@@ -6,7 +6,8 @@ if [[ $JAPR_WHEEL == "1" ]]; then
   if [[ $JAPR_OS == "Linux" ]]; then
     docker run --rm -w /io -v `pwd`:/io quay.io/pypa/manylinux1_x86_64 /opt/python/cp35-cp35m/bin/python setup.py bdist_wheel
     docker run --rm -w /io -v `pwd`:/io quay.io/pypa/manylinux1_x86_64 /opt/python/cp36-cp36m/bin/python setup.py bdist_wheel
-    docker run --rm -w /io -v `pwd`:/io quay.io/pypa/manylinux1_x86_64 auditwheel repair dist/*.whl
+    docker run --rm -w /io -v `pwd`:/io quay.io/pypa/manylinux1_x86_64 auditwheel repair dist/*-cp35-cp35m-linux_x86_64.whl
+    docker run --rm -w /io -v `pwd`:/io quay.io/pypa/manylinux1_x86_64 auditwheel repair dist/*-cp36-cp36m-linux_x86_64.whl
     rm dist/*.whl
     ls -lha wheelhouse
     unzip -l wheelhouse/*.whl
