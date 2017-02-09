f1252392ab027abe1a49c460420d091c944927b5
Squeaky <squeaky_pl@gmx.com>
2017-02-09 09:46:03 -0200

    source install

--- misc/travis/script.sh
+++ misc/travis/script.sh
@@ -2,10 +2,6 @@
 
 set -ex
 
-if [[ $JAPR_OS == "Darwin" ]]; then
-  export PATH=/Users/travis/build/squeaky-pl/japronto/venv/bin:$PATH
-fi
-
 if [[ $JAPR_WHEEL == "1" ]]; then
   if [[ $JAPR_OS == "Linux" ]]; then
     docker run --rm -u `id -u` -w /io -v `pwd`:/io quay.io/pypa/manylinux1_x86_64 /opt/python/$PYTHON_TAG/bin/python setup.py bdist_wheel
